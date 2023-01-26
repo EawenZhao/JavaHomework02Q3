3a1c64164217ea3de6f483ab4d4739619357b59c
zyh19 <zyh1999zyh@yeah.net>
2023-01-26 21:54:18 +0800

    fix 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter();
-        counter.count(new File("C:\\Users\\zyh19\\IdeaProjects\\JavaHomework02Q3\\src\\News.txt"), new File("./Result.txt"));
+        counter.count(new File("src/News.txt"), new File("src/Result.txt"));
     }
 }
